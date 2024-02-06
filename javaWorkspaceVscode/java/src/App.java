d8753d9c0726e2e25cfc9df86b75e0a3ea1f89a2
Cauã Felipe <111020928+FreameCorleone@users.noreply.github.com>
2024-02-06 11:42:26 -0300

    first on vscode 3

---
+++ javaWorkspaceVscode/java/src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Qual o seu nome? ");
+        String nome = scan.nextLine();
+        System.out.println("Seu nome é " + nome);
+
+
+
+
+    }
+}
