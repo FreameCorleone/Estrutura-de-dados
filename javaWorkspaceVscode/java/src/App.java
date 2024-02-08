056fa3aa80815854a78cb533b25e30ff3305d803
Cauã Felipe <111020928+FreameCorleone@users.noreply.github.com>
2024-02-08 10:34:04 -0300

    on vscorde 4

--- javaWorkspaceVscode/java/src/App.java
+++ javaWorkspaceVscode/java/src/App.java
@@ -5,10 +5,20 @@ public class App {
         Scanner scan = new Scanner(System.in);
         System.out.println("Qual o seu nome? ");
         String nome = scan.nextLine();
+        if (nome.equals("Enrico")) {
+            System.out.println("Gay!");
+        }
         System.out.println("Seu nome é " + nome);
+        
+        System.out.println("E a sua idade? ");
+        int idade = scan.nextInt();
 
+       
 
-
-
+        for(int i = 0; i < 10; i++){
+            idade = idade * 2;
+            System.out.println("ida " + idade);
+        }
+        scan.close();
     }
 }
---
+++ javaWorkspaceVscode/java/src/rt.java
@@ -0,0 +1,5 @@
+public class rt {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
